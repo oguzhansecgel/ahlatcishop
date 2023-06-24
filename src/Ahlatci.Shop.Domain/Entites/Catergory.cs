fe2d1a24be2c877e1bbf9dc029e8fb9b18fc58ea
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-06-24 17:59:03 +0300

    Entity katmanında Category ve Product entityler oluşturuldu.
    Entity katmanında Category ve Product entityler oluşturuldu.

---
+++ src/Ahlatci.Shop.Domain/Entites/Catergory.cs
@@ -0,0 +1,18 @@
+﻿using Ahlatci.Shop.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahlatci.Shop.Domain.Entites
+{
+    public class Catergory : AuditableEntity
+    {
+
+        public string Name { get; set; }
+
+ 
+       
+    }
+}
---
+++ src/Ahlatci.Shop.Domain/Entites/Product.cs
@@ -0,0 +1,21 @@
+﻿using Ahlatci.Shop.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahlatci.Shop.Domain.Entites
+{
+    public class Product : AuditableEntity
+    {
+
+        public int CatergoryId { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public int UnitInStock { get; set; }
+        public Decimal UnitPrice{ get; set; }
+       
+        
+    }
+}
